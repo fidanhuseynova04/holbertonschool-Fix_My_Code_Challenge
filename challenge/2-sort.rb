097b5ad20bb5834a662dd3376bf4b7ef7221eea0
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-09-15 18:27:19 +0000

    2-sort.rb

---
+++ challenge/2-sort.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+###
+# Sort integer arguments (ascending)
+###
+
+result = []
+ARGV.each do |arg|
+    # skip if not integer
+    next unless arg =~ /^-?\d+$/
+
+    # convert to integer
+    i_arg = arg.to_i
+
+    # insert result at the right position
+    inserted = false
+    i = 0
+    while i < result.size do
+        if result[i] > i_arg
+            result.insert(i, i_arg)
+            inserted = true
+            break
+        end
+        i += 1
+    end
+    result << i_arg unless inserted
+end
+
+puts result
